c0e6e0f70df781888b728e54a310179ba8e2490f
alex-d-bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2026-07-01 17:54:32 +0100

    feat: a fresh new start

--- learning-node-js/public/qa/tests-global.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Alex on 2/9/16.
- */
-suite('Global Tests', function(){
-    test('page has a valid title', function(){
-        assert(document.title && document.title.match(/\S/) &&
-                document.title.toUpperCase() !== 'TODO');
-    });
-});
