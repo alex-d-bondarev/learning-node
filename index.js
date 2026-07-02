c0c9e671287b526b233a92821558fb8695591895
alex-d-bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2026-07-02 13:18:33 +0100

    feat: basic API

--- index.js
+++ index.js
@@ -1,29 +1,90 @@
+require("dotenv").config()
+
 const express = require("express")
 const mongoose = require("mongoose")
-require("dotenv").config()
 
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
+const bookSchema = mongoose.Schema({
+    bookName: {
+        type: String,
+        required: true,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+    }
+})
+
+BookModel = mongoose.model("Book", bookSchema)
+
+app.post("/books", async (req, res) => {
+    try {
+        const newBook = await BookModel.create(req.body)
+        res.status(201).json(newBook)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+app.get("/books", async (req, res) => {
+    try {
+        const bookList = await BookModel.find()
+        res.status(200).send(bookList)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+app.get("/books/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        const book = await BookModel.findById(id)
+
+        if (!book) {
+            res.status(404).json({message: `Book with ID ${id} not found`})
+        }
+
+        res.status(200).json(book)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+app.delete("/books/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        const deletedBook = await BookModel.findByIdAndDelete(id)
+
+        if (!deletedBook) {
+            res.status(404).json({message: `Book with ID ${id} not found`})
+        }
+        res.status(200).json({message: `Book with ID ${id} deleted successfully`})
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+app.put("/books/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        const updatedBook = await BookModel.findByIdAndUpdate(id, req.body, {new: true})
+
+        if (!updatedBook) {
+            res.status(404).json({message: `Book with ID ${id} not found`})
+        }
+        res.status(200).json({message: `Book with ID ${id} updated successfully`, updatedBook})
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
 mongoose.connect(process.env.MONGO_CONNECTION_STRING)
     .then(() => console.log("MongoDB Connected!"))
     .catch((error) => console.log(error));
-
-app.get("/", (req, res) => {
-    res.send("Hello page")
-})
-
-app.get("/about", (req, res) => {
-    res.send("About page")
-})
-
-app.get("/contact", (req, res) => {
-    res.send("Contact page")
-})
-
-app.get("/home", (req, res) => {
-    res.send("Home page")
-})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
